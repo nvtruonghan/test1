9a4853caafd87b63a308aba1c1c3d45e0191c1d1
nvtruonghan <usefree27051995@gmail.com>
2021-07-07 15:25:49 +0700

    test lan 4

--- code.c
+++ code.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main(){
 	for(int i =1; i<11; i++){
-		printf("i = %d \n",i);
+		//printf("i = %d \n",i);
 		//printf("nvth \n");
-		//printf("ptcq \n"):
+		printf("ptcq \n"):
 	}
 
 	return(0);

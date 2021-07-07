178a0f2e4fbf9093f78be58b3e49ccc75a52dde9
nvtruonghan <usefree27051995@gmail.com>
2021-07-07 15:35:21 +0700

    test lan6

--- code.c
+++ code.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 int main(){
 	for(int i =1; i<11; i++){
-		//printf("i = %d \n",i);
+		printf("i = %d \n",i);
 		printf("nvth \n");
-		printf("ptcq \n"):
+		printf("ptcq \n");
+		printf("nts \n");
 	}
 
 	return(0);
